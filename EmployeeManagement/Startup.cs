11b1373de475344ba6231043f9adfad120ade448
Waqas Sial <waqas4400@hotmail.com>
2020-02-02 00:05:17 +0500

    use default file with static web pages using DefaultFileOptions class

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -1,14 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace EmployeeManagement
 {
@@ -27,32 +22,29 @@ namespace EmployeeManagement
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Use(async (context, next) =>
-            {
-                logger.LogInformation("MW1: Icoming Request");
-                await next();
-                logger.LogInformation("MW1: OutGoing Request");
-            });
+            DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();
+            defaultFilesOptions.DefaultFileNames.Clear();
+            defaultFilesOptions.DefaultFileNames.Add("foo.html");
 
-            app.Use(async (context, next) =>
-            {
-                logger.LogInformation("MW2: Icoming Request");
-                await next();
-                logger.LogInformation("MW2: OutGoing Request");
-            });
+            app.UseDefaultFiles(defaultFilesOptions);
+            app.UseStaticFiles();
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("MW3: Request Handled and Result Produced");
-                logger.LogInformation("MW3: Request Handled and Result Produced");
+                await context.Response.WriteAsync("Hello World");
             });
+
+
+
+
+
             //app.UseRouting();
 
             //app.UseEndpoints(endpoints =>
