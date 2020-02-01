ae9588939e936a95753d70679cac2fcf7631c4d5
Waqas Sial <waqas4400@hotmail.com>
2020-02-02 02:14:00 +0500

    Add MVC To The Application

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -20,6 +20,7 @@ namespace EmployeeManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,34 +28,12 @@ namespace EmployeeManagement
         {
             if (env.IsDevelopment())
             {
-                DeveloperExceptionPageOptions developerExceptionPageOptions = new DeveloperExceptionPageOptions
-                {
-                    SourceCodeLineCount = 10
-                };
-                app.UseDeveloperExceptionPage(developerExceptionPageOptions);
+                app.UseDeveloperExceptionPage();
             }
-            //file app.UseFileServer performs both UseDefaultFile() and UseStaticFile() actions
-            app.UseFileServer();
 
-            app.Run(async (context) =>
-            {
-                throw new Exception("Some Error");
-                //await context.Response.WriteAsync("Hello World");
-            });
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
 
-
-
-
-
-            //app.UseRouting();
-
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapGet("/", async context =>
-            //    {
-            //        await context.Response.WriteAsync("Hello World");
-            //    });
-            //});
         }
     }
 }
