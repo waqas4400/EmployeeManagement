892f3736fcf36df75ac67656b97f12e51ac2f56c
Waqas Sial <waqas4400@hotmail.com>
2020-02-05 11:27:04 +0500

    Added Validation to Form

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -38,10 +38,14 @@ namespace EmployeeManagement.Controllers
         }
 
         [HttpPost]
-        public RedirectToActionResult Create(Employee employee)
+        public IActionResult Create(Employee employee)
         {
-            Employee newEmployee = _employeeRepository.Add(employee);
-            return RedirectToAction("Details", new { id = newEmployee.Id });
+            if (ModelState.IsValid)
+            {
+                Employee newEmployee = _employeeRepository.Add(employee);
+                return RedirectToAction("Details", new { id = newEmployee.Id });
+            }
+            return View();
         }
     }
 }
--- EmployeeManagement/Models/Employee.cs
+++ EmployeeManagement/Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,13 @@ namespace EmployeeManagement.Models
     public class Employee
     {
         public int Id { get; set; }
+
+        [Required, MaxLength(50)]
         public String Name { get; set; }
+
+        [Display(Name="Office Email")]
+        [RegularExpression(@"^[a - zA - Z0 - 9_.+ -] +@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", ErrorMessage ="Invalid Email Address Format")]
+        [Required]
         public String Email { get; set; }
         public Dept Department { get; set; }
     }
